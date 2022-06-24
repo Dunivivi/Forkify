99aa5860965a8e256abe9638f762deaa3ed07aac
dunivivi <tudor.duca@isa.utm.md>
2022-06-24 18:33:04 +0300

    Bug fixed

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '2a2a23f4-1846-4e70-8a95-fd0b1fe38868';
 export const MODAL_CLOSE_SEC = 2.5;
